0ac4adbeb4cf5c7feafee21971b1f32272b72222
venkatesh-tc <venkatesh@targetclose.com>
2024-12-10 18:05:48 +0530

    Custom login feature

---
+++ CustomLoginRegister/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginRegisterController;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('register', [LoginRegisterController::class, 'register'])->name('register');
+    Route::post('register', [LoginRegisterController::class, 'registerPost'])->name('register.post');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+    Route::resource('products', ProductController::class);
+});
